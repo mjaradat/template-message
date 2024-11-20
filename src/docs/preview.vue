3cddab869cc25a7a1245dfdea253e585d167647c
mjaradat <tjaradat1@gmail.com>
2024-11-20 12:25:10 +0300

    -remove files

--- src/docs/preview.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-preview />
-</template>
-
-<script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
-
-const VPreview = defineAsyncComponent(() => import('../components/VPreview.vue'))
-</script>
--- src/router.ts
+++ src/router.ts
@@ -2,15 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import CreateLayout from './layouts/CreateLayout.vue'
 import VRouterView from './components/VRouterView.vue'
 
-import Input from './docs/input.vue' // Example component, adjust as needed
-import Layout from './layouts/Layout.vue'
-import Select from './docs/select.vue'
-import VisualSelect from './docs/visual-select.vue'
-import RadioButtons from './docs/radio-buttons.vue'
-import Preview from './docs/preview.vue'
-import Records from './docs/records.vue'
-import ImageU from './docs/image.vue'
-
 const routes = [
   {
     path: '/',
@@ -21,48 +12,6 @@ const routes = [
         path: '', // default route
         name: 'new-template-message',
         component: CreateLayout
-      }
-    ]
-  },
-  {
-    path: '/docs',
-    name: 'docs',
-    component: Layout,
-    children: [
-      {
-        path: '/input',
-        name: 'input',
-        component: Input
-      },
-      {
-        path: '/select',
-        name: 'select',
-        component: Select
-      },
-      {
-        path: '/visual-select',
-        name: 'visual-select',
-        component: VisualSelect
-      },
-      {
-        path: '/radio-buttons',
-        name: 'radio-buttons',
-        component: RadioButtons
-      },
-      {
-        path: '/preview',
-        name: 'preview',
-        component: Preview
-      },
-      {
-        path: '/buttons',
-        name: 'buttons',
-        component: Records
-      },
-      {
-        path: '/image',
-        name: 'image',
-        component: ImageU
       }
     ]
   }
